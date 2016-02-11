e47c713cf0b652db9dc1558e37204bcb2fd3063d
DEGABGED <franciszac.dlc@gmail.com>
2016-02-11 22:56:50 +0800

    I did something

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,5 +2,6 @@ class ProfilesController < ApplicationController
     before_action :authenticate_user!
 
     def show
+	@user_name = current_user.user_name
     end
 end
--- db/migrate/20160208071204_add_user_name_to_users.rb
+++ db/migrate/20160208071204_add_user_name_to_users.rb
@@ -2,4 +2,6 @@ class AddUserNameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :user_name, :string
   end
+  
+  add_index :users, :user_name, unique: true
 end
