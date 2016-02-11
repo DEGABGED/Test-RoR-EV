eb6c2068d12b4c292edf04664ba0aa8560c1ea73
DEGABGED <franciszac.dlc@gmail.com>
2016-02-11 12:43:26 +0800

    Error Fix in database migration. Ignore if no such problems occur in Ubuntu

--- db/migrate/20160208071204_add_user_name_to_users.rb
+++ db/migrate/20160208071204_add_user_name_to_users.rb
@@ -2,6 +2,4 @@ class AddUserNameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :user_name, :string
   end
-
-  add_index :users, :user_name, unique: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208071204) do
+ActiveRecord::Schema.define(version: 20160211043811) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
