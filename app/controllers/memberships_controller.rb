4f241f10d1840a116652ed5f2bcd1047ee033b17
DEGABGED <franciszac.dlc@gmail.com>
2016-02-13 08:19:04 +0800

    forgot to pull

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -10,4 +10,22 @@ class MembershipsController < ApplicationController
             @memberships = Membership.where(user_id: current_user.id).find_each
         end
     end
+    
+    def create
+        @mem = Membership.create(group_id: params[:group_id], user_id: current_user.id)
+        if @mem.save
+            flash[:success] = "Group followed!"
+        else
+            flash[:alert] = "Something went wrong!"
+        end
+    end
+
+    def destroy
+        @mem = Membership.where(user_id: current_user.id, group_id: params[:group_id]).find
+        if @mem.destroy
+            flash[:success] = "Group unfollowed!"
+        else
+            flash[:alert] = "Something went wrong!"
+        end
+    end
 end
